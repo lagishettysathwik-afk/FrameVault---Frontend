0e0259d586cb8e75f0d7062619d093bc1d6037cf
lagishettysathwik-afk <lagishettysathwik@gmail.com>
2026-03-17 15:53:45 +0530

    change backend url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1';
+const API_BASE_URL = 'https://framevault-backend.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
