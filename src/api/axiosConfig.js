629af29ee4552e714f1d95ca39af751d9e912257
lagishettysathwik-afk <lagishettysathwik@gmail.com>
2026-03-24 10:53:26 +0530

    fixed axios base url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://framevault-backend.onrender.com';
+const API_BASE_URL = 'https://framevault-backend.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
